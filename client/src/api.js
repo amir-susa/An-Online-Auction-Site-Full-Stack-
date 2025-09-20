c742c99bc50889f19c2e205bce6e306ff6afd565
Amir Rahemeto <amiroimage@gmail.com>
2025-09-20 17:13:16 +0300

    Update API base URL to use Render deployment link

--- client/src/api.js
+++ client/src/api.js
@@ -2,15 +2,15 @@ import axios from 'axios';
 
 // Create an Axios instance with a base URL
 const API = axios.create({
-  baseURL: 'http://localhost:3000', // This is the URL of your Koa.js backend
+  **baseURL: 'https://an-online-auction-site-full-stack-1.onrender.com',** // <-- Updated to your Render deployment URL
 });
 
 
 // Optionally attach token automatically for all requests
 API.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
+  const token = localStorage.getItem('token');
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
 });
 
 export default API;
